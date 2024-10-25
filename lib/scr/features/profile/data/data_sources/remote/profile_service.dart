ed250f97471a7bb2578eb29c9683b7f20daaf94a
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-10-24 23:01:18 -0500

    .

--- lib/scr/features/profile/data/data_sources/remote/profile_service.dart
+++ lib/scr/features/profile/data/data_sources/remote/profile_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:trabajo_moviles_ninjacode/scr/core/utils/usecases/jwt_storage.dart';
 
 class ProfileService {
-  final String baseUrl = 'http://localhost:8080/api/v1';
+  final String baseUrl = 'https://hormonal-care-backend-production.up.railway.app/api/v1';
 
   Future<Map<String, dynamic>> fetchProfileDetails(int userId) async {
     final token = await JwtStorage.getToken();
