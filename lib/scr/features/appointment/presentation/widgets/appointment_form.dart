eab91c6aab913b5f320f5b0d9700012cda172151
Jose Calderon <u202213076@upc.edu.pe>
2025-06-23 16:21:38 -0500

    refactor: Improve code formatting and readability in appointment_form.dart

--- lib/scr/features/appointment/presentation/widgets/appointment_form.dart
+++ lib/scr/features/appointment/presentation/widgets/appointment_form.dart
@@ -13,14 +13,16 @@ class JitsiMeetingLinkGenerator {
 
   static String generateMeetingLink({String? roomPrefix}) {
     final String randomString = _generateRandomString(10);
-    final String roomName = roomPrefix != null ? '$roomPrefix-$randomString' : randomString;
+    final String roomName =
+        roomPrefix != null ? '$roomPrefix-$randomString' : randomString;
     return '$_baseUrl$roomName';
   }
 
   static String _generateRandomString(int length) {
     const String chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
     final Random random = Random();
-    return List.generate(length, (index) => chars[random.nextInt(chars.length)]).join();
+    return List.generate(length, (index) => chars[random.nextInt(chars.length)])
+        .join();
   }
 }
 
@@ -43,7 +45,8 @@ class _AppointmentFormState extends State<AppointmentForm> {
 
   DateTime? _selectedDate;
 
-  final MedicalAppointmentRepository repository = MedicalAppointmentRepository(MedicalAppointmentApi());
+  final MedicalAppointmentRepository repository =
+      MedicalAppointmentRepository(MedicalAppointmentApi());
 
   @override
   void initState() {
@@ -60,7 +63,7 @@ class _AppointmentFormState extends State<AppointmentForm> {
   }
 
   Future<bool> _isTimeSlotAvailable(String startTime, String endTime) async {
-    final userId = await JwtStorage.getUserId();
+    // final userId = await JwtStorage.getUserId();
     final role = await JwtStorage.getRole();
 
     if (role != 'ROLE_DOCTOR') {
@@ -68,12 +71,16 @@ class _AppointmentFormState extends State<AppointmentForm> {
     }
 
     final existingAppointments = await repository.fetchAppointmentsForToday();
-    final newStart = DateTime.parse("${_selectedDate!.toIso8601String().split('T')[0]} $startTime:00");
-    final newEnd = DateTime.parse("${_selectedDate!.toIso8601String().split('T')[0]} $endTime:00");
+    final newStart = DateTime.parse(
+        "${_selectedDate!.toIso8601String().split('T')[0]} $startTime:00");
+    final newEnd = DateTime.parse(
+        "${_selectedDate!.toIso8601String().split('T')[0]} $endTime:00");
 
     for (var appointment in existingAppointments) {
-      final existingStart = DateTime.parse("${appointment['eventDate']} ${appointment['startTime']}:00");
-      final existingEnd = DateTime.parse("${appointment['eventDate']} ${appointment['endTime']}:00");
+      final existingStart = DateTime.parse(
+          "${appointment['eventDate']} ${appointment['startTime']}:00");
+      final existingEnd = DateTime.parse(
+          "${appointment['eventDate']} ${appointment['endTime']}:00");
 
       if (newStart.isBefore(existingEnd) && newEnd.isAfter(existingStart)) {
         return false;
@@ -89,7 +96,8 @@ class _AppointmentFormState extends State<AppointmentForm> {
 
       if (!await _isTimeSlotAvailable(startTime, endTime)) {
         Navigator.pop(context);
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('The selected time slot is not available.')));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('The selected time slot is not available.')));
         return;
       }
 
@@ -100,19 +108,23 @@ class _AppointmentFormState extends State<AppointmentForm> {
         'startTime': startTime,
         'endTime': endTime,
         'title': _titleController.text,
-        'description': JitsiMeetingLinkGenerator.generateMeetingLink(roomPrefix: _titleController.text),
+        'description': JitsiMeetingLinkGenerator.generateMeetingLink(
+            roomPrefix: _titleController.text),
         'doctorId': userId,
         'patientId': widget.patientId,
         'color': '0xFF039BE5', // Default color
       };
 
-      final success = await repository.createMedicalAppointment(appointmentData);
+      final success =
+          await repository.createMedicalAppointment(appointmentData);
       if (success) {
         Navigator.pop(context);
         _confettiController.play();
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Medical appointment created successfully!')));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Medical appointment created successfully!')));
       } else {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to create medical appointment.')));
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Failed to create medical appointment.')));
       }
     }
   }
@@ -146,12 +158,13 @@ class _AppointmentFormState extends State<AppointmentForm> {
   @override
   Widget build(BuildContext context) {
     final limaTimeZone = tz.getLocation('America/Lima');
-    final now = tz.TZDateTime.now(limaTimeZone);
+    // final now = tz.TZDateTime.now(limaTimeZone);
 
     return SingleChildScrollView(
       child: ConstrainedBox(
         constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height * 0.6, // Ajusta el valor según sea necesario
+          maxHeight: MediaQuery.of(context).size.height *
+              0.6, // Ajusta el valor según sea necesario
         ),
         child: Form(
           key: _formKey,
@@ -166,14 +179,14 @@ class _AppointmentFormState extends State<AppointmentForm> {
                   decoration: InputDecoration(
                     labelText: 'Title',
                     hintText: 'Title of the meeting',
-                    prefixIcon: Icon(Icons.title),
+                    prefixIcon: const Icon(Icons.title),
                     filled: true,
-                    fillColor: Color(0xFFF5F5F5),
+                    fillColor: const Color(0xFFF5F5F5),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
-                  style: TextStyle(fontSize: 14),
+                  style: const TextStyle(fontSize: 14),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter the title of the meeting';
@@ -181,7 +194,7 @@ class _AppointmentFormState extends State<AppointmentForm> {
                     return null;
                   },
                 ),
-                SizedBox(height: 12),
+                const SizedBox(height: 12),
 
                 // Field: Date
                 TextFormField(
@@ -189,14 +202,14 @@ class _AppointmentFormState extends State<AppointmentForm> {
                   decoration: InputDecoration(
                     labelText: 'Date',
                     hintText: 'Day',
-                    prefixIcon: Icon(Icons.calendar_today),
+                    prefixIcon: const Icon(Icons.calendar_today),
                     filled: true,
-                    fillColor: Color(0xFFF5F5F5),
+                    fillColor: const Color(0xFFF5F5F5),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
-                  style: TextStyle(fontSize: 14),
+                  style: const TextStyle(fontSize: 14),
                   readOnly: true,
                   onTap: () => _selectDate(context),
                   validator: (value) {
@@ -204,16 +217,18 @@ class _AppointmentFormState extends State<AppointmentForm> {
                       return 'Please select a date';
                     }
                     if (_selectedDate != null) {
-                      final selectedDateInLima = tz.TZDateTime.from(_selectedDate!, limaTimeZone);
+                      final selectedDateInLima =
+                          tz.TZDateTime.from(_selectedDate!, limaTimeZone);
                       final nowInLima = tz.TZDateTime.now(limaTimeZone);
-                      if (selectedDateInLima.isBefore(nowInLima.subtract(Duration(days: 1)))) {
+                      if (selectedDateInLima.isBefore(
+                          nowInLima.subtract(const Duration(days: 1)))) {
                         return 'The date cannot be in the past';
                       }
                     }
                     return null;
                   },
                 ),
-                SizedBox(height: 12),
+                const SizedBox(height: 12),
 
                 // Field: Time "From"
                 Row(
@@ -224,18 +239,18 @@ class _AppointmentFormState extends State<AppointmentForm> {
                         decoration: InputDecoration(
                           labelText: 'From',
                           hintText: 'Hour',
-                          prefixIcon: Icon(Icons.access_time),
+                          prefixIcon: const Icon(Icons.access_time),
                           filled: true,
-                          fillColor: Color(0xFFF5F5F5),
+                          fillColor: const Color(0xFFF5F5F5),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
                         ),
-                        style: TextStyle(fontSize: 14),
+                        style: const TextStyle(fontSize: 14),
                         validator: _validateTime,
                       ),
                     ),
-                    SizedBox(width: 12),
+                    const SizedBox(width: 12),
 
                     // Field: Time "To"
                     Expanded(
@@ -244,21 +259,27 @@ class _AppointmentFormState extends State<AppointmentForm> {
                         decoration: InputDecoration(
                           labelText: 'To',
                           hintText: 'Hour',
-                          prefixIcon: Icon(Icons.access_time),
+                          prefixIcon: const Icon(Icons.access_time),
                           filled: true,
-                          fillColor: Color(0xFFF5F5F5),
+                          fillColor: const Color(0xFFF5F5F5),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
                         ),
-                        style: TextStyle(fontSize: 14),
+                        style: const TextStyle(fontSize: 14),
                         validator: (value) {
                           final error = _validateTime(value);
                           if (error != null) return error;
-                          if (_fromTimeController.text.isNotEmpty && value != null) {
-                            final fromTime = _fromTimeController.text.split(':').map(int.parse).toList();
-                            final toTime = value.split(':').map(int.parse).toList();
-                            final from = DateTime(0, 0, 0, fromTime[0], fromTime[1]);
+                          if (_fromTimeController.text.isNotEmpty &&
+                              value != null) {
+                            final fromTime = _fromTimeController.text
+                                .split(':')
+                                .map(int.parse)
+                                .toList();
+                            final toTime =
+                                value.split(':').map(int.parse).toList();
+                            final from =
+                                DateTime(0, 0, 0, fromTime[0], fromTime[1]);
                             final to = DateTime(0, 0, 0, toTime[0], toTime[1]);
                             if (to.isBefore(from)) {
                               return 'End time must be after start time';
@@ -270,7 +291,7 @@ class _AppointmentFormState extends State<AppointmentForm> {
                     ),
                   ],
                 ),
-                SizedBox(height: 16),
+                const SizedBox(height: 16),
 
                 // Custom buttons (Clear and Create event)
                 CustomButtons(
