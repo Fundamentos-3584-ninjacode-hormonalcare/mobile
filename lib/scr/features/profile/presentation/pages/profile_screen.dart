66e5de954fafc103046612d923a58916396ff5b4
Jose Calderon <u202213076@upc.edu.pe>
2025-06-23 15:47:49 -0500

    refactor: Improve profile data loading and clean up unused image upload code

--- lib/scr/features/profile/presentation/pages/profile_screen.dart
+++ lib/scr/features/profile/presentation/pages/profile_screen.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:mime/mime.dart';
 import 'package:http_parser/http_parser.dart';
+import 'package:trabajo_moviles_ninjacode/scr/features/profile/data/data_sources/remote/profile_service.dart';
 import '../../../../core/utils/usecases/jwt_storage.dart';
 import '../widgets/profile_picture_widget.dart';
 import '../widgets/profile_field_widget.dart';
 import '../widgets/logout_button_widget.dart';
 import '../widgets/save_cancel_buttons_widget.dart';
 import '../widgets/edit_mode_widget.dart';
-
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -29,8 +28,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Future<void> _loadProfileData() async {
     try {
-      int userId = await JwtStorage.getUserId();
-      final data = await ProfileService().fetchProfileByUserId(userId);
+      int? userId = await JwtStorage.getUserId();
+      final data = await ProfileService().fetchProfileDetails(userId!);
       setState(() {
         profileData = data;
       });
@@ -38,6 +37,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       print('Error loading profile: $e');
     }
   }
+
   Future<void> _pickAndUploadImageFromFile(File file) async {
     if (profileData == null) return;
 
@@ -50,7 +50,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
         await http.MultipartFile.fromPath(
           'file',
           file.path,
-          contentType: MediaType.parse(lookupMimeType(file.path) ?? 'image/jpeg')!,
+          contentType:
+              MediaType.parse(lookupMimeType(file.path) ?? 'image/jpeg'),
         ),
       );
 
@@ -64,36 +65,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
     }
   }
 
+  // Future<void> _pickAndUploadImage() async {
+  //   final picker = ImagePicker();
+  //   final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
-  Future<void> _pickAndUploadImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+  //   if (pickedFile != null && profileData != null) {
+  //     final file = File(pickedFile.path);
+  //     final profileId = profileData!['id'];
 
-    if (pickedFile != null && profileData != null) {
-      final file = File(pickedFile.path);
-      final profileId = profileData!['id'];
+  //     final uri =
+  //         Uri.parse('http://10.0.2.2:8080/api/v1/profile/$profileId/image');
+  //     final request = http.MultipartRequest('PUT', uri)
+  //       ..headers['Authorization'] = 'Bearer ${await JwtStorage.getToken()}'
+  //       ..files.add(
+  //         await http.MultipartFile.fromPath(
+  //           'file',
+  //           file.path,
+  //           contentType:
+  //               MediaType.parse(lookupMimeType(file.path) ?? 'image/jpeg')!,
+  //         ),
+  //       );
 
-      final uri = Uri.parse('http://10.0.2.2:8080/api/v1/profile/$profileId/image');
-      final request = http.MultipartRequest('PUT', uri)
-        ..headers['Authorization'] = 'Bearer ${await JwtStorage.getToken()}'
-        ..files.add(
-          await http.MultipartFile.fromPath(
-            'file',
-            file.path,
-            contentType: MediaType.parse(lookupMimeType(file.path) ?? 'image/jpeg')!,
-          ),
-        );
-
-      final response = await request.send();
-      if (response.statusCode == 200) {
-        final body = await response.stream.bytesToString();
-        print('Imagen subida exitosamente: $body');
-        await _loadProfileData(); // Refrescar con nueva imagen
-      } else {
-        print('Fallo al subir imagen: ${response.statusCode}');
-      }
-    }
-  }
+  //     final response = await request.send();
+  //     if (response.statusCode == 200) {
+  //       final body = await response.stream.bytesToString();
+  //       print('Imagen subida exitosamente: $body');
+  //       await _loadProfileData(); // Refrescar con nueva imagen
+  //     } else {
+  //       print('Fallo al subir imagen: ${response.statusCode}');
+  //     }
+  //   }
+  // }
 
   void toggleEditMode() {
     setState(() {
@@ -104,22 +106,22 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     if (profileData == null) {
-      return Scaffold(body: Center(child: CircularProgressIndicator()));
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF6A828D),
-        title: Text('Account'),
+        backgroundColor: const Color(0xFF6A828D),
+        title: const Text('Account'),
         centerTitle: true,
-        titleTextStyle: TextStyle(
+        titleTextStyle: const TextStyle(
           color: Colors.white,
           fontSize: 20.0,
           fontWeight: FontWeight.bold,
         ),
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
@@ -128,32 +130,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  icon: Icon(Icons.edit, color: Colors.black),
+                  icon: const Icon(Icons.edit, color: Colors.black),
                   onPressed: toggleEditMode,
                 ),
-                SizedBox(width: 8.0),
+                const SizedBox(width: 8.0),
                 ProfilePictureWidget(
                   isEditing: isEditing,
                   toggleEditMode: toggleEditMode,
                   imageUrl: profileData!['image'],
                   onImageSelected: (file) => _pickAndUploadImageFromFile(file),
                 ),
-                SizedBox(width: 8.0),
-                LogoutButtonWidget(),
+                const SizedBox(width: 8.0),
+                const LogoutButtonWidget(),
               ],
             ),
 
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
 
             if (!isEditing) ...[
-              ProfileFieldWidget(label: "First name", value: profileData!['firstName'] ?? ''),
-              ProfileFieldWidget(label: "Last name", value: profileData!['lastName'] ?? ''),
-              ProfileFieldWidget(label: "Gender", value: profileData!['gender'] ?? ''),
-              ProfileFieldWidget(label: "Birthday", value: profileData!['birthday'] ?? ''),
-              ProfileFieldWidget(label: "Phone number", value: profileData!['phoneNumber'] ?? ''),
-              ProfileFieldWidget(label: "Email", value: profileData!['user']?['email'] ?? ''),
-              ProfileFieldWidget(label: "Medical license number", value: profileData!['licenseNumber'] ?? '---'),
-              ProfileFieldWidget(label: "Subspecialty", value: profileData!['subspecialty'] ?? '---'),
+              ProfileFieldWidget(
+                  label: "First name", value: profileData!['firstName'] ?? ''),
+              ProfileFieldWidget(
+                  label: "Last name", value: profileData!['lastName'] ?? ''),
+              ProfileFieldWidget(
+                  label: "Gender", value: profileData!['gender'] ?? ''),
+              ProfileFieldWidget(
+                  label: "Birthday", value: profileData!['birthday'] ?? ''),
+              ProfileFieldWidget(
+                  label: "Phone number",
+                  value: profileData!['phoneNumber'] ?? ''),
+              ProfileFieldWidget(
+                  label: "Email", value: profileData!['user']?['email'] ?? ''),
+              ProfileFieldWidget(
+                  label: "Medical license number",
+                  value: profileData!['licenseNumber'] ?? '---'),
+              ProfileFieldWidget(
+                  label: "Subspecialty",
+                  value: profileData!['subspecialty'] ?? '---'),
             ] else ...[
               EditModeWidget(
                 profile: profileData!,
@@ -167,7 +180,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
             ],
 
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
 
             if (isEditing)
               SaveCancelButtonsWidget(
